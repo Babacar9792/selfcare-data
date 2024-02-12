fc4d33dc780560fa75f085fc125bac46cbafaee7
M.doucoure <mouhameddouc12@gmail.com>
2024-02-12 13:36:52 +0000

    CRUD departement back

---
+++ SelfCareData_laravel/app/Http/Requests/DepartementRequest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Traits\ResponseTrait;
+use Illuminate\Http\Response;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class DepartementRequest extends FormRequest
+{
+    use ResponseTrait;
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'libelle' => 'required|string|max:20|unique:departements',
+            'name' => "required"
+        ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        $errors = $validator->errors()->all();
+        $message = implode(', ', $errors);
+        throw new HttpResponseException(
+            $this->responseData($message, false, Response::HTTP_BAD_REQUEST, [])
+        );
+    }
+
+
+    public function messages(){
+        return [
+            "libelle.required"=> "Le libelle est requis",
+            "libelle.max"=> "Le libelle ne doit pas contenir plus de  20 caractere",
+            "libelle.unique" => "Le libelle existe déja"
+        ];
+    }
+}
